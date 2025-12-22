8db2e04bf50eb53f2553f1be7fae2480e352ed14
Fariha <fariha1032004@gmail.com>
2025-12-22 22:09:18 +0600

    Fixed CategoryProducts.fxml and CategoryProductsController.java

---
+++ src/main/java/com/example/finding_bd_products/CategoryProductsController.java
@@ -0,0 +1,88 @@
+package com.example.finding_bd_products;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CategoryProductsController {
+    @FXML
+    private Label categoryTitle;
+
+    @FXML
+    private Label categoryDescription;
+
+    @FXML
+    private GridPane productsGrid;
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private Button homeBtn;
+
+    @FXML
+    private Button categoriesBtn;
+
+    @FXML
+    private Button newlyAddedBtn;
+
+    @FXML
+    private Button favouritesBtn;
+
+
+    @FXML
+    protected void showHome() {
+        loadPage("Home.fxml");
+    }
+
+    @FXML
+    protected void showCategories() {
+        loadPage("Categories.fxml");
+    }
+
+    @FXML
+    protected void showNewlyAdded() {
+        loadPage("NewlyAdded.fxml");
+    }
+
+    @FXML
+    protected void showFavourites() {
+        loadPage("Favourites.fxml");
+    }
+
+    @FXML
+    protected void onSearch() {
+        String searchText = searchField.getText();
+    }
+
+    private void loadPage(String fxmlFile) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) homeBtn.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
