851b218cd293846a8cef1a067489d0acb439ecda
Fariha <fariha1032004@gmail.com>
2025-12-28 20:37:54 +0600

    Modified HelloApplication.java

--- src/main/java/com/example/finding_bd_products/HelloApplication.java
+++ src/main/java/com/example/finding_bd_products/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
-        stage.setTitle("Finding BD Products");
+        stage.setTitle("Finding BD Products - Login");
         stage.setScene(scene);
         stage.show();
     }
